3e33f520ec31c84f06a6163974da2eeb47e3790c
dom <soaresdominic@gmail.com>
2018-03-05 18:12:55 +0000

    Added training and test set validation, with multiple iterations

--- kmeans.py
+++ kmeans.py
@@ -4,6 +4,9 @@ import os
 import numpy as np
 import pandas as pd
 from statistics import mode
+from statistics import mean
+import random
+import copy
 
 import sklearn
 from sklearn.cluster import KMeans
@@ -16,9 +19,12 @@ import matplotlib.cm as cm
 def main():
     dataFrame = import_data()
     dataList, dataClasses = cleanData(dataFrame)
-    predLabels = kmeans(dataList)
-    validation(predLabels)
-
+    accuracies = []
+    for i in range(10):
+        predLabels = kmeans(dataList)
+        accuracies.append(validation(predLabels, len(predLabels)))
+    print(accuracies)
+    print("The average accuracy is: " + str(mean(accuracies)))
 
 
 def import_data():
@@ -47,43 +53,56 @@ def kmeans(data):
     # Initialize the clusterer to make 3 clusters
     # seed of 10 for reproducibility.
     clusterer = KMeans(n_clusters=3, random_state=10)
-    cluster_labels = clusterer.fit_predict(data)
+    train, test = seperateData(data)
+    clusterer.fit(train)  #train
+    cluster_labels = clusterer.predict(test)  #test
     cluster_labels_list = cluster_labels.tolist()
-    #print(type(cluster_labels_list))
+    #print(cluster_labels_list)
     return cluster_labels_list
 
 
+#data is a list of lists
+def seperateData(data):
+    data = data.tolist()
+    dataSeperate = [data[:70],data[70:140], data[140:]]
+    test = []
+    for d in dataSeperate:  #3 sets of 70 instances
+        for i in range(10):  #grab 10 from each set
+            r = random.randint(0,len(d)-1)
+            test.append(d.pop(r))
+    #print(data)
+    #print(test)
+    return data, test
 
-def validation(labels):
+
+
+
+def validation(labels, size):
     #k means error reporting
     #find the most popular per 70
     #print(type(cluster_labels))
-    mode1 = mode(labels[:70])
-    mode2 = mode(labels[70:140])
-    mode3 = mode(labels[140:])
+
+    n1 = int(size/3)
+    n2 = n1*2
+
+    mode1 = mode(labels[:n1])
+    mode2 = mode(labels[n1:n2])
+    mode3 = mode(labels[n2:])
     #print(mode1,mode2,mode3)
 
     Ttrue = 0
     Tfalse = 0
-    Ttrue += labels[:70].count(mode1)
-    Tfalse += 70-labels[:70].count(mode1)
-    Ttrue += labels[70:140].count(mode2)
-    Tfalse += 70-labels[70:140].count(mode2)
-    Ttrue += labels[140:].count(mode3)
-    Tfalse += 70-labels[140:].count(mode3)
+    Ttrue += labels[:n1].count(mode1)
+    Tfalse += n1-labels[:n1].count(mode1)
+    Ttrue += labels[n1:n2].count(mode2)
+    Tfalse += n1-labels[n1:n2].count(mode2)
+    Ttrue += labels[n2:].count(mode3)
+    Tfalse += n1-labels[n2:].count(mode3)
 
-    print(str(Ttrue) + "/" + str(Ttrue + Tfalse))
-    print("Accuracy: " + str(Ttrue / (Ttrue + Tfalse)))
+    #print(str(Ttrue) + "/" + str(Ttrue + Tfalse))
+    #print("Accuracy: " + str(Ttrue / (Ttrue + Tfalse)))
     #print(labels)
-
-    """
-    print(labels[:70].count(mode1))
-    print(70-labels[:70].count(mode1))
-    print(labels[70:140].count(mode2))
-    print(70-labels[70:140].count(mode2))
-    print( labels[140:].count(mode3))
-    print(70-labels[140:].count(mode3))
-    """
+    return (Ttrue / (Ttrue + Tfalse))
 
 
 main()
